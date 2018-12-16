ede396520b9f159f9ade758da189cb49b4a21f7c
Allan Olson <allanmolson@gmail.com>
2018-12-16 12:10:04 -0600

    MyLibrary Project

---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    { 
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+
+        public virtual LibraryCard LibraryCard { get; set; } //Ctrl + K to comment out a line, Ctrl + u to uncomment
+        public virtual LibraryBranch HomeLibraryBranch { get; set; }
+    }
+}
---
+++ MyLibrary/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using LibraryData.Models;
+using Microsoft.AspNetCore.Mvc;
+using MyLibrary.Models;
+
+namespace MyLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(model: new Models.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
